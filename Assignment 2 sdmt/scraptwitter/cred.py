5ae9e27e616c15cf0aed4c4827f9072b4dc823ec
ammy07 <200454325@georgianc.on.ca>
2021-02-24 20:45:20 -0500

    first commit

---
+++ Assignment 2 sdmt/scraptwitter/cred.py
@@ -0,0 +1,15 @@
+keys = {
+    "Access token" : "1316925471397797889-ZYTtmcOz10hjV3hp7heqhvNRP4oJXg",
+ "Access token secret" : "FVcPse05J5f457MLSCKUvCUvVNRgNXKSFnXs2iKP7Vmn4",
+    "API key" : "rISmNJYyRBgU2jxv4XrRhfsKc",
+    "API key secret" : "NjvRet5Uzll45iroCgdOat81opTdDTpV0caUZDv1TCmAGblxLR",
+    "userID" :"@amritkaurarora"
+  }
+
+redit_keys = {
+    "client_id":"hzb07uAY4bxZGQ",
+    "client_secret":"guRW6KjYOHPq4GKMbxeib3XZu7G9Ew",
+    "user_agent":"script by u/Bikramchopra96",
+    "redirect_uri":"http://localhost:8080",
+    "refresh_token":"823977672566-rXrpMTL9Or_aQMmA-1LP8QgHvQK6Dg"
+}
